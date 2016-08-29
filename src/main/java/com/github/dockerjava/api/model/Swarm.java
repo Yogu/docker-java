1373d1ac5aea03074ff17e49e04f1be0a5215309
Jan Melcher <mail@jan-melcher.de>
2016-08-29 13:14:13 +0200

    Add swarm property to Info command result

--- src/main/java/com/github/dockerjava/api/model/Swarm.java
+++ src/main/java/com/github/dockerjava/api/model/Swarm.java
@@ -1,97 +1,20 @@
 package com.github.dockerjava.api.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.github.dockerjava.core.RemoteApiVersion;
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
 
 import javax.annotation.CheckForNull;
-import java.io.Serializable;
 import java.util.Date;
 
-
 /**
- * Used for creating swarms.
- *
- * @since {@link RemoteApiVersion#VERSION_1_24}
+ * @since 1.24
  */
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class Swarm implements Serializable {
-
-    public static final Long serialVersionUID = 1L;
-
-    /**
-     * @since 1.24
-     */
-    @JsonProperty("CreatedAt")
-    private Date createdAt;
-
-    /**
-     * @since 1.24
-     */
-    @JsonProperty("Spec")
-    private SwarmSpec spec;
+public class Swarm extends ClusterInfo {
 
     /**
      * @since 1.24
      */
     @JsonProperty("JoinTokens")
     private SwarmJoinTokens joinTokens;
-
-    /**
-     * @since 1.24
-     */
-    @JsonProperty("ID")
-    private String id;
-
-    /**
-     * @since 1.24
-     */
-    @JsonProperty("UpdatedAt")
-    private Date updatedAt;
-
-    /**
-     * @since 1.24
-     */
-    @JsonProperty("Version")
-    private ResourceVersion version;
-
-    /**
-     * @see #createdAt
-     */
-    @CheckForNull
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    /**
-     * @see #createdAt
-     */
-    public Swarm withCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-        return this;
-    }
-
-    /**
-     * @see #spec
-     */
-    @CheckForNull
-    public SwarmSpec getSpec() {
-        return spec;
-    }
-
-    /**
-     * @see #spec
-     */
-    public Swarm withSpec(SwarmSpec spec) {
-        this.spec = spec;
-        return this;
-    }
 
     /**
      * @see #joinTokens
@@ -109,67 +32,33 @@ public class Swarm implements Serializable {
         return this;
     }
 
-    /**
-     * @see #id
-     */
-    @CheckForNull
-    public String getId() {
-        return id;
+    @Override
+    public Swarm withCreatedAt(Date createdAt) {
+        super.withCreatedAt(createdAt);
+        return this;
     }
 
-    /**
-     * @see #id
-     */
+    @Override
     public Swarm withId(String id) {
-        this.id = id;
+        super.withId(id);
         return this;
     }
 
-    /**
-     * @see #updatedAt
-     */
-    @CheckForNull
-    public Date getUpdatedAt() {
-        return updatedAt;
+    @Override
+    public Swarm withSpec(SwarmSpec spec) {
+        super.withSpec(spec);
+        return this;
     }
 
-    /**
-     * @see #updatedAt
-     */
+    @Override
     public Swarm withUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
+        super.withUpdatedAt(updatedAt);
         return this;
     }
 
-    /**
-     * @see #version
-     */
-    @CheckForNull
-    public ResourceVersion getVersion() {
-        return version;
-    }
-
-    /**
-     * @see #version
-     */
+    @Override
     public Swarm withVersion(ResourceVersion version) {
-        this.version = version;
+        super.withVersion(version);
         return this;
     }
-
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        return EqualsBuilder.reflectionEquals(this, o);
-    }
-
-    @Override
-    public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
-    }
-
 }
